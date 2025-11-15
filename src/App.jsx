c18710d6297bdffb20302ab233075d3aa109ae62
SouravM16 <souravmalviya08@gmail.com>
2025-11-15 19:33:37 +0530

    added airdrop component and changed app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+//import React, { useMemo } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+//import { clusterApiUrl } from "@solana/web3.js";
+import "@solana/wallet-adapter-react-ui/styles.css";
+
+
+function App() {
+ // const network = WalletAdapterNetwork.Devnet;
+
+  //const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  return (
+    <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+      <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <WalletMultiButton />
+            <WalletDisconnectButton />
+          </div>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
+
+export default App;
